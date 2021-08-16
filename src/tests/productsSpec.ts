ca562f42ed8eea2c8101a97d8ed2ebe67e5217b3
Moschops <evolaric@gmail.com>
2021-08-16 02:03:39 -0700

    Jasmine testing now working for database models

--- src/tests/productsSpec.ts
+++ src/tests/productsSpec.ts
@@ -2,8 +2,13 @@ import { ProductStore } from '../models/products';
 
 const store = new ProductStore();
 
-describe('Products model testing', () => {
-  it('should have an index method', () => {
+describe('Products model testing', (): void => {
+  it('should have an index method', (): void => {
     expect(store.index()).toBeDefined;
+  });
+
+  it('index method should return a list of products', async (): Promise<void> => {
+    const result = await store.index();
+    expect(result).toEqual([]);
   });
 });
